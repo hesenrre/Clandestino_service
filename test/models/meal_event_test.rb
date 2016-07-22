19f203504d6af1a34a5f98c9b0c73ee27f88fe70
Demetrio Cruz <hesenrre@gmail.com>
2016-07-21 19:32:44 -0500

    added relation test for MealEvents

--- test/models/meal_event_test.rb
+++ test/models/meal_event_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class MealEventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "MealEvent has correct relations" do
+    event = MealEvent.first
+    assert event, "event should exists"
+    assert (event.meal_attendants and not event.meal_attendants.empty?),
+    "event should have attendants"
+    assert event.clients.first,
+    "attendant must have client related"
+    assert (event.event_chefs and not event.event_chefs.empty?),
+    "event should have event chefs"
+    assert event.chefs.first,
+    "event should have chefs"
+    assert event.event_galleries,
+    "event should have galleries"
+    assert event.event_galleries.first.event_media_group,
+    "event should have related event media group"
+    assert event.address,
+    "event should have an address related"
+    assert (event.event_providers and not event.event_providers.empty?),
+    "event should have an event providers related"
+  end
 end
