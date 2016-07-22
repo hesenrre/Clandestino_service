4fb526a752248e14831449ae59d4aa991fd94d97
Demetrio Cruz <hesenrre@gmail.com>
2016-07-21 19:31:37 -0500

    added configuration to silence timezone deprecation message

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,7 @@ module Clandestino
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Mexico City'
+    config.active_record.time_zone_aware_types = [:datetime,:time]
   end
 end
