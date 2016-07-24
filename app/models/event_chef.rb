d9c772378f63cb02e558d557ed9d84b8daf08d05
Demetrio Cruz <hesenrre@gmail.com>
2016-07-24 18:22:47 -0500

    added deep relations to person

--- app/models/event_chef.rb
+++ app/models/event_chef.rb
@@ -1,5 +1,6 @@
 class EventChef < ApplicationRecord
   enum status: [:confirmed, :not_going, :maybe]
   belongs_to :chef
+  has_one :person, through: :chef
   belongs_to :meal_event
 end
--- app/models/meal_attendant.rb
+++ app/models/meal_attendant.rb
@@ -2,5 +2,6 @@ class MealAttendant < ApplicationRecord
   enum status: [:confirmed, :not_going, :maybe]
   belongs_to :meal_event
   belongs_to :client
+  has_one :person, through: :client
   has_one :meal_event_command
 end
