94f12c71cff70eeb17ecb067deb872a994678b68
Demetrio Cruz <hesenrre@gmail.com>
2016-08-04 15:14:26 -0500

    removed unused constraints

--- db/migrate/20160804003446_add_resource_owner_id_to_client.rb
+++
@@ -1,5 +0,0 @@
-class AddResourceOwnerIdToClient < ActiveRecord::Migration[5.0]
-  def change
-    add_column :clients, :resource_owner_id, :integer
-  end
-end
--- db/migrate/20160804003535_add_resource_owner_id_to_chefs.rb
+++
@@ -1,5 +0,0 @@
-class AddResourceOwnerIdToChefs < ActiveRecord::Migration[5.0]
-  def change
-    add_column :chefs, :resource_owner_id, :integer
-  end
-end
