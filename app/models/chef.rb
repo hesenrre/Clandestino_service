0ac250cb5fbebe4e382c4be8367be604e38566d9
Demetrio Cruz <hesenrre@gmail.com>
2016-08-04 15:18:41 -0500

    added active call to chef

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -11,4 +11,15 @@ class Chef < ApplicationRecord
   has_many :chef_galleries
   has_many :phones, through: :person
   # has_many :chef_media_groups, through: :chef_galleries
+
+  def active_call
+    return is_on_time(calls.last) ? calls.last : nil 
+  end
+
+  private
+  def is_on_time(call)
+     not (call.meal_event.closed? or call.meal_event.canceled?) and
+    ((call.unconfirmed? and call.meal_event.confirmation_deadline.future?) or
+    (call.confirmed? and Time.now < call.meal_event.event_date + 6.hours ))
+  end
 end
