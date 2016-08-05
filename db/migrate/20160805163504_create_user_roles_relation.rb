6aa7bd16a158ba739296549575fb90cb4d2dcdd5
Demetrio Cruz <hesenrre@gmail.com>
2016-08-05 14:14:15 -0500

    Changed table name to complain rails standard

--- db/migrate/20160805163504_create_user_roles_relation.rb
+++ db/migrate/20160805163504_create_user_roles_relation.rb
@@ -1,6 +1,6 @@
 class CreateUserRolesRelation < ActiveRecord::Migration[5.0]
   def change
-    create_table :user_roles do |t|
+    create_table :roles_users do |t|
       t.belongs_to :user, index: true
       t.belongs_to :role, index: true
     end
