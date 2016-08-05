a775787db35d9e21b8e7734990d15c400c80851d
Demetrio Cruz <hesenrre@gmail.com>
2016-08-05 13:38:02 -0500

    changed relations on meal event models

--- app/models/meal_event.rb
+++ app/models/meal_event.rb
@@ -3,12 +3,12 @@ class MealEvent < ApplicationRecord
 
   belongs_to :address
   has_many :invitations
-  has_many :clients, through: :invitations
+  has_many :clients, through: :invitations, class_name: 'User'
   has_many :calls
-  has_many :chefs, through: :calls
+  has_many :chefs, through: :calls, class_name: 'User'
   has_many :event_providers
   has_many :providers, through: :event_providers
-  has_many :event_galleries
   has_many :event_products
   has_many :products, through: :event_products
+  has_and_belongs_to_many :galleries
 end
