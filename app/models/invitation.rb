7124565e4dc02f0a3e2f26519d5c8c5582c28cb9
Demetrio Cruz <hesenrre@gmail.com>
2016-08-05 14:24:52 -0500

    changed to unified user model

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -1,7 +1,6 @@
 class Invitation < ApplicationRecord
   enum status: [:unconfirmed, :confirmed, :not_going, :maybe]
   belongs_to :meal_event
-  belongs_to :client
-  has_one :person, through: :client
+  belongs_to :user
   has_one :meal_event_command
 end
