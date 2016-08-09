a57eed705a42bb296b10760c388d10aafaf6aba4
Demetrio Cruz <hesenrre@gmail.com>
2016-08-09 17:03:28 -0500

    changed strategy for checking authorized roles for user

--- app/controllers/api/v1/me_controller.rb
+++ app/controllers/api/v1/me_controller.rb
@@ -1,57 +1,40 @@
 class Api::V1::MeController < Api::V1::BaseController
-  devise_group :user, contains: [:client, :chef]
-  before_action :authenticate_user!, only: [:index, :phones]
-  before_action :authenticate_client!, except: [:index, :calls, :phones]
-  before_action :authenticate_chef!, only: [:calls]
-  before_action :me
+  before_action :authenticate_user!
+  before_action -> {authorized_roles :clients}
 
   def index
     @me = current_user
-    @user_type = user_type
+    # @user_type = user_type
   end
 
   # BEGIN CLIENT METHODS
   def interests
-    @interests = @me.interests
+    @interests = current_user.interests
   end
 
   def food_preferences
     # p current_client
-    @food_preferences = @me.food_styles
+    @food_preferences = current_user.food_styles
   end
 
   def delivery_addresses
-    @addresses = @me.delivery_addresses
+    @addresses = current_user.delivery_addresses
   end
 
   def invitations
-    @invitations = @me.invitations
+    @invitations = current_user.invitations
   end
   # END CLIENT METHODS
 
   # BEGIN CHEF METHODS
   def calls
-    @calls = @me.calls
+    @calls = current_user.calls
   end
   # END CHEF METHODS
 
   # BEGIN GLOBAL METHODS
   def phones
-    @phones = @me.phones
+    @phones = current_user.phones
   end
   # END GLOBAL METHODS
-
-
-  private
-  def me
-    @me = current_user || current_client || current_chef
-  end
-
-  def user_type
-    if current_client
-      "client"
-    else
-      "chef"
-    end
-  end
 end
