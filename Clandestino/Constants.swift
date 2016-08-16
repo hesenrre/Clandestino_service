cee6cbdd2410df9311334bfac09922125b07cad3
Demetrio Cruz <hesenrre@gmail.com>
2016-08-16 13:40:13 -0500

    added constants to declare global constants and functions

---
+++ Clandestino/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Clandestino
+//
+//  Created by Demetrio Cruz on 8/15/16.
+//  Copyright © 2016 Roonin Labs S de RL de CV. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let _SERVICE_HOST = "1abb954c.ngrok.io"
+}
